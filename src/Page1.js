d8d070665ce2df64e7796076e600756bf0c56745
tanoask <akshaytan2@gmail.com>
2019-06-15 15:23:35 +0530

    added a module

--- src/Page1.js
+++ src/Page1.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import './App.css';
 class Page1 extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+
   render() {
     return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
+      <div className="pageBody">
+        <h1>Enter data for us to Start</h1>
+        <form>
+     <label>
+       Name:
+       <input type="text" name="name" />
+     </label>
+     <input type="submit" value="Submit" />
+</form>
       </div>
     );
   }
 }
+
+export default Page1;
