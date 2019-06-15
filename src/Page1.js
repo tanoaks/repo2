a8b6db3ab8ff2d59508c09cbc4a0dcaedb06492a
tanoask <akshaytan2@gmail.com>
2019-06-15 14:20:20 +0530

    inital

---
+++ src/Page1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+class Page1 extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
